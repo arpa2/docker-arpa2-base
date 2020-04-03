3d99907d53abeb5a417fe8c6df0e461a3e97fd0c
Henri Manson <hfmanson@gmail.com>
2020-04-03 10:57:10 +0200

    volume improvements

--- build-diameter/setup-volume.sh
+++ build-diameter/setup-volume.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/vanrein/quick-der /usr/local/src/quick-der.git
-mkdir /usr/local/src/quick-der.git/build
-cd /usr/local/src/quick-der.git/build
-cmake -DDEBUG:BOOL=OFF ..
-make -C /usr/local/src/quick-der.git/build all install
-python /usr/local/src/quick-der.git/setup.py install
-python -m easy_install arpa2.quickder_tools
-python -m easy_install arpa2.quickder
+rm -r /usr/local/src/kip.git/*
 git clone -b sxover-diameter https://gitlab.com/arpa2/kip /usr/local/src/kip.git
 mkdir /usr/local/src/kip.git/build
 cd /usr/local/src/kip.git/build
