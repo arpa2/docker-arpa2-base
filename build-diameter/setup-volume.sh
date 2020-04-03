24a9febfc187353ae900faf2b28784d5d38cff3e
Henri Manson <hfmanson@gmail.com>
2020-04-03 11:36:32 +0200

    improvements

--- build-diameter/setup-volume.sh
+++ build-diameter/setup-volume.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-rm -r /usr/local/src/kip.git/*
-git clone -b sxover-diameter https://gitlab.com/arpa2/kip /usr/local/src/kip.git
-mkdir /usr/local/src/kip.git/build
-cd /usr/local/src/kip.git/build
-cmake ..
-make -C /usr/local/src/kip.git/build
-cp /usr/local/src/kip.git/test/bin/root.key /etc/unbound/root.key
-#cd /usr/local/src/kip.git/build ; ctest
-cp /usr/local/src/kip.git/build/src/kip  /usr/local/bin/kip
-cp /usr/local/src/kip.git/build/src/kipd /usr/local/bin/kipd
+DIR="/usr/local/src/kip.git"
+if [ ! -d "$DIR"/.git ]; then
+	git clone -b sxover-diameter https://gitlab.com/arpa2/kip $DIR
+	mkdir $DIR/build
+	cd $DIR/build
+	cmake -DDEBUG:BOOL=ON ..
+	make
+	#cd $DIR/build ; ctest
+fi
+make -C $DIR/build install
+mkdir -p /etc/unbound
+cp $DIR/test/bin/root.key /etc/unbound/root.key
+cp $DIR/build/src/kip  /usr/local/bin/kip
+cp $DIR/build/src/kipd /usr/local/bin/kipd
